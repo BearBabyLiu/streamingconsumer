3ad1835336ac45b0b32fd6fd69e0223df7bbcfc4
Liu Huang <liu.huang@zte.com.cn>
2018-11-14 19:50:30 +0800

    增加入ES代码

--- src/scala/com/bigdata/KafkaConsumer.scala
+++ src/scala/com/bigdata/KafkaConsumer.scala
@@ -4,19 +4,30 @@ import org.apache.spark.SparkConf
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.spark.streaming.dstream.InputDStream
 import org.apache.spark.streaming.kafka010._
 import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
 import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
+import org.apache.spark.streaming.kafka010.ConsumerStrategies.Assign
+import org.elasticsearch.spark.streaming.EsSparkStreaming
+
+import scala.collection.mutable
 
 /**
   * Created by Administrator on 2018/11/10.
   */
 object KafkaConsumer {
-  val conf = new SparkConf().setMaster("local[2]").setAppName("KafkaCosumer")
+  var kafkaStream: InputDStream[ConsumerRecord[String, String]]
+
+  val conf = new SparkConf().setMaster("local[2]").setAppName("KafkaCosumer").
+    set("es.nodes", "192.168.1.1").
+    set("es.port", "9200").
+    set("es.index.auto.create", "true")
+
   val ssc = new StreamingContext(conf, Seconds(1))
 
   val kafkaParams = Map[String, Object](
-    "bootstrap.servers" -> "localhost:9092,anotherhost:9092",
+    "bootstrap.servers" -> "localhost:9092",
     "key.deserializer" -> classOf[StringDeserializer],
     "value.deserializer" -> classOf[StringDeserializer],
     "group.id" -> "kafka-consumer",
@@ -24,10 +35,32 @@ object KafkaConsumer {
     "enable.auto.commit" -> (false: java.lang.Boolean)
   )
 
-  val topics = Array("topicA", "topicB")
-  val stream = KafkaUtils.createDirectStream[String, String](
-    ssc,
-    PreferConsistent,
-    Subscribe[String, String](topics, kafkaParams)
-  )
+  val topics = Array("topicName")
+  val zkInstance = ZookeeperService("topicName", "master:2181")
+  val fromOffsets = zkInstance.calculateCunsumerOffset("brokeHost", 9092)
+  if (fromOffsets.isEmpty) {
+    kafkaStream = KafkaUtils.createDirectStream[String, String](
+      ssc,
+      PreferConsistent,
+      Subscribe[String, String](topics, kafkaParams)
+  } else {
+    kafkaStream = KafkaUtils.createDirectStream[String, String](
+      ssc,
+      PreferConsistent,
+      Assign[String, String](fromOffsets.keys.toList, kafkaParams, fromOffsets))
+  }
+
+  /**
+    * kafka的数据入ES
+    */
+  kafkaStream.foreachRDD(rdd => {
+    /**
+      * json流也可以直接入es，也可以在这里转换
+      */
+    val message = rdd.map(record => record.value())
+    val microbatches = mutable.Queue(message)
+    val dstream = ssc.queueStream(microbatches)
+    EsSparkStreaming.saveToEs(dstream, "spark/docs")
+    zkInstance.savePartitionOffsets(rdd)
+  })
 }
